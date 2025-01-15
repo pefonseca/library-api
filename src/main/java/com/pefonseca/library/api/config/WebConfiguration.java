3849a7b40e769061b65daa2e561d929256fccd2c
Pedro Fonseca <pedrofonunes@gmail.com>
2025-01-15 11:10:40 -0300

    feat: att spring

---
+++ src/main/java/com/pefonseca/library/api/config/WebConfiguration.java
@@ -0,0 +1,20 @@
+package com.pefonseca.library.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration implements WebMvcConfigurer {
+
+    // Configuração de paginas web .
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+}
